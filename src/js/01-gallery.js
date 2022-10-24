f2b56598947e8a359ab68e14b12aa82214324770
Anna-Polishchyk <aniapolischyk@gmail.com>
2022-10-24 22:00:36 +0300

    js galery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,15 +1,15 @@
 // Add imports above this line
-// import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
-// console.log(galleryItems);
+console.log(galleryItems);
 
 // Add imports above this line
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import { galleryItems } from './gallery-items';
+// import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
